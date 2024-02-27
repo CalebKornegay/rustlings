049e862e0161186a438633c9740b54351ca31895
CalebKornegay <caleb.kornegay@gmail.com>
2024-02-27 18:54:10 -0500

    Exercise 48

--- exercises/09_strings/strings3.rs
+++ exercises/09_strings/strings3.rs
@@ -3,21 +3,25 @@
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 fn trim_me(input: &str) -> String {
-    // TODO: Remove whitespace from both ends of a string!
-    ???
+    let s: String = input.to_string();
+    let s = s.trim().to_owned();
+    println!("{s} {input}");
+    s
 }
 
 fn compose_me(input: &str) -> String {
-    // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    let s: String = input.to_string();
+    let s = s + " world!";
+    s
 }
 
 fn replace_me(input: &str) -> String {
-    // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    let s: String = input.to_string();
+    let s = s.replace("cars", "balloons").to_owned();
+    println!("{s}");
+    s
 }
 
 #[cfg(test)]
